0a350082f7bcda54fec5720a625b02bf34bc55ef
artpoddybnyy <artpoddybnyy@gmail.com>
2018-01-28 10:10:03 +0200

    add story for users coords

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 var server = require('ws').Server;
-var s = new server({port: process.env.PORT || 3000});
+var s = new server({port: process.env.PORT || 1000});
 
 
+var connectedUsers = [{userName: "center of the city", lat: "49.9935", lng: "36.230382999"}];
 
 s.on('connection', function (ws) {
-
+    if (connectedUsers.length > -1) {
+        ws.send(JSON.stringify(connectedUsers));
+    }
     ws.on('message', function (message) {
-       s.clients.forEach(function (client) {
-           if (client !== ws){
-           client.send(message)
-           }
-       });
-        ws.send(message);
+        s.clients.forEach(function (client) {
+            // if (client !== ws){
+            var parse = JSON.parse(message);
+            var searchUser = connectedUsers.map(function (item) {
+                return item.userName
+            }).indexOf(parse.userName);
+            if (searchUser === -1) {
+                connectedUsers.push(parse);
+                client.send(message);
+            } else if (searchUser > -1) {
+                connectedUsers[searchUser].lat = +parse.lat;
+                connectedUsers[searchUser].lng = +parse.lng;
+                client.send(message);
+            }
+        });
     });
 
 
-    ws.on("error", function(error){
+    ws.on("error", function (error) {
         // console.log(error);
     });
 
